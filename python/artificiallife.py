5cc76b32464f5b0d93b05d0da4cca3f6cd68e0df
Rob Weber <robweberjr@gmail.com>
2019-05-26 22:03:26 +0000

    added artificial life functions, right now only makes default lifeforms move

---
+++ python/artificiallife.py
@@ -0,0 +1,172 @@
+import random
+
+#the main class that handles each lifeforms initialisation, movement, colour, expiry and statistics
+class lifeForm(object):
+  
+    #standard class initialisation
+    def __init__(self, Id,globalAggression,globalTTL):
+        self.Id = Id
+        self.globalAggression = globalAggression
+        self.globalTTL = globalTTL
+  
+    #when this function is called it gives the lifeform of the class instance its properties from the random numbers inserted into it
+    def sparkLife(self, seed, seed2, seed3, startX, startY):
+        self.lifeSeed = seed
+        self.lifeSeed2 = seed2
+        self.lifeSeed3 = seed3
+        #lifeseed 1 controls the random number generation for the red colour, maximum aggression factor starting direction and maximum possible lifespan
+        random.seed(self.lifeSeed)
+        self.redColor = random.randint(1, 255)
+        self.maxAggressionFactor = random.randint(1, self.globalAggression)
+        self.direction = random.randint(1, 9)
+        self.maxLife = random.randint(1, self.globalTTL)
+        #lifeseed 2 controls the random number generation for the green colour, aggression factor between 0 and the maximum from above as well as the time the entity takes to change direction
+        random.seed(self.lifeSeed2)
+        self.greenColor = random.randint(1, 255)
+        self.aggressionFactor = random.randint(0, self.maxAggressionFactor)   
+        self.timeToMove = random.randint(1, 25)
+        self.timeToMoveCount = self.timeToMove
+        #lifeseed 3 controls the random number generation for the green colour, and time to live between 0 and the maximum from above
+        random.seed(self.lifeSeed3)
+        self.blueColor = random.randint(1, 255)
+        self.timeToLive = random.randint(0, self.maxLife)
+        self.timeToLiveCount = self.timeToLive
+        #reset the global random seed
+        random.seed()
+    
+        #set the starting location of the lifeform from the x and y positions passed into the function
+        self.matrixPositionX = startX
+        self.matrixPositionY = startY
+
+    #when called this function will display the statistics of the current lifeform in the main loop 
+    def getStats(self):
+        print ('ID: ' + str(self.Id))
+        print ('Seed 1: ' + str(self.lifeSeed))
+        print ('Seed 2: ' + str(self.lifeSeed2))
+        print ('Seed 3: ' + str(self.lifeSeed3))
+        print ('Direction: ' + str(self.direction))
+        print ('Time to move total: ' + str(self.timeToMove))
+        print ('Time to next move: ' + str(self.timeToMoveCount))
+        print ('Total lifetime: ' + str(self.timeToLive))
+        print ('Time left to live: ' + str(self.timeToLiveCount))
+        print ('Aggression Factor: ' + str(self.aggressionFactor))
+        print ('Position X: ' + str(self.matrixPositionX))
+        print ('Position Y: ' + str(self.matrixPositionY))
+        print ('Color: ' + 'R-' + str(self.redColor) + ' G-' + str(self.greenColor) + ' B-' + str(self.blueColor) + '\n')
+  
+    #this function will move the entity in its currently set direction (with 8 possible directions), if it hits the edge of the board it will then assign a new random direction to go in, this function also handles the time to move count which when hits 0 will select a new random direction for the entity regardless of whether it has hit the edge of the board or another entity
+    def movement(self):
+    
+        #if the edge of the board is not hit and direction is '1' then move the entity up the X axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity
+        if self.direction == 1:
+            if self.matrixPositionX < 7:
+                self.matrixPositionX += 1
+        else:
+            self.direction = self.randomiseDirection()
+    
+        #if the edge of the board is not hit and direction is '2' then move the entity down the X axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity   
+        if self.direction == 2:
+            if self.matrixPositionX > 0:
+                self.matrixPositionX -= 1
+        else:
+            self.direction = self.randomiseDirection()
+        
+        #if the edge of the board is not hit and direction is '3' then move the entity up the Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity   
+        if self.direction == 3:
+            if self.matrixPositionY < 7:
+                self.matrixPositionY += 1
+        else:
+            self.direction = self.randomiseDirection()
+    
+        #if the edge of the board is not hit and direction is '4' then move the entity down the Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity   
+        if self.direction == 4:
+            if self.matrixPositionY > 0:
+                self.matrixPositionY -= 1
+        else:
+            self.direction = self.randomiseDirection()
+
+        #if the edge of the board is not hit and direction is '5' then move the entity up the X and Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity
+        if self.direction == 5:
+            if self.matrixPositionX < 7 and self.matrixPositionY < 0:
+                self.matrixPositionX += 1
+                self.matrixPositionY += 1
+        else:
+            self.direction = self.randomiseDirection()
+
+        #if the edge of the board is not hit and direction is '6' then move the entity down the X and Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity       
+        if self.direction == 6:
+            if self.matrixPositionX > 0 and self.matrixPositionY > 0:
+                self.matrixPositionX -= 1
+                self.matrixPositionY -= 1
+            else:
+                self.direction = self.randomiseDirection()
+
+        #if the edge of the board is not hit and direction is '7' then move the entity down the X axis and up Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity       
+        if self.direction == 7:
+            if self.matrixPositionY < 7 and self.matrixPositionX > 0:
+                self.matrixPositionX -= 1
+                self.matrixPositionY += 1
+            else:
+                self.direction = self.randomiseDirection()
+
+         #if the edge of the board is not hit and direction is '8' then move the entity up the X axis and down Y axis by 1, if it has hit the edge of the board its direction is randomised by the randomisedirection function being called for the entity           
+        if self.direction == 8:
+            if self.matrixPositionY > 0 and self.matrixPositionX < 7:
+                self.matrixPositionX += 1
+                self.matrixPositionY -= 1
+            else:
+                self.direction = self.randomiseDirection()
+    
+        #if the direction is '9' do not move the entity   
+        elif self.direction == 9:
+            self.matrixPositionX = self.matrixPositionX
+            self.matrixPositionY = self.matrixPositionY
+    
+        #minus 1 from the time to move count until it hits 0, at which point the entity will change direction from the randomisedirecion function being called
+        if self.timeToMoveCount > 0:
+            self.timeToMoveCount -= 1
+        elif self.timeToMoveCount <= 0:
+            self.timeToMoveCount = self.timeToMove
+            self.direction = self.randomiseDirection()
+      
+    #when called this function with select a random new direction for the lifeform that is not the direction it is already going    
+    def randomiseDirection(self):
+    
+        r = range(1,self.direction) + range(self.direction+1, 10)
+        return random.choice(r)
+  
+    #this function counts down a lifeforms time to live from its full lifetime assigned to it when a lifeforms time to live hits zero remove it from the list of lifeforms and set the colours to 0, 0, 0
+    def expireEntity(self, iListIn):
+    
+        if self.timeToLiveCount > 0:
+            self.timeToLiveCount -= 1
+            return (iListIn)
+        elif self.timeToLiveCount <= 0:
+            self.redColor = 0
+            self.greenColor = 0
+            self.blueColor = 0
+            iListIn.remove(self.Id)
+            return (iListIn)
+  
+    #function to call for erasing an entity from the board as well as the main list for lifeforms
+    def killEntity(self, iListIn):
+        self.redColor = 0
+        self.greenColor = 0
+        self.blueColor = 0
+        iListIn.remove(self.Id)
+        return (iListIn)
+      
+    #function to call for erasing an entity from the board by fading it away as well as removing from the main list for lifeforms
+    def fadeEntity(self, iListIn):
+        for c in range(0, 255):
+            if (self.redColor > 0):
+                self.redColor -= 1
+            if (self.greenColor > 0):
+                self.greenColor -= 1
+            if (self.blueColor > 0):
+                self.blueColor -= 1
+            unicorn.set_pixel(self.matrixPositionX, self.matrixPositionY, self.redColor, self.greenColor, self.blueColor)
+            unicorn.show()
+        iListIn.remove(self.Id)
+        return (iListIn)
+    
--- python/service.py
+++ python/service.py
@@ -1,6 +1,12 @@
 import webiopi
 import json
 import sys
+import random
+import time
+import os
+import numpy as np
+import unicornhat as unicorn
+from operator import itemgetter
 
 #do this to import local modules
 module_path = '/home/pi/Artificial_Life/python'
@@ -10,10 +16,13 @@ if module_path not in sys.path:
 #import custom classes
 import constants
 from settings import Settings
+from artificiallife import lifeForm
 
 #### GLOBAL VARIABLES
 is_running = False
 settings = None
+iList = None
+holder = None
 
 #### WEB MACROS START HERE
 
@@ -51,17 +60,100 @@ def checkRunning():
 def changeRunning(action):
     global is_running
     is_running = (True if action == 'true' else False)
-    webiopi.info(action)
+
     response = {'message':'','is_running':is_running}
 
-    if(action):
+    if(is_running):
         response['message'] = 'Starting Artificial Life'
+        setup_game() #startup the game variables here
     else:
         response['message'] = 'Stopping Artificial Life'
 
     return json.dumps(response)
 
 #### WEB MACROS END HERE
+
+#print the number of lifeforms currently on the board
+def printLifeformNo(no):
+    webiopi.info("Lifeforms: " + str(no))
+
+#draw the position and colour of the current lifeform onto the board
+def drawLEDS(x, y, r, g, b):
+    unicorn.set_pixel(x, y, r, g, b)
+    #unicorn.show()
+
+#clear the unicorn hat led grid 
+def clearLEDS():
+    unicorn.clear()
+
+#function used for generating a random number to be used as a seed, this is used to generate all 3 lifeseeds resulting in 1.e+36 possible types of lifeform
+def genRandom():
+    return random.randint(1, 1000000000000)
+
+#function used to determine whether a lifeform is colliding with another currently on the board 
+def collisionDetector(boardPositions, posX, posY, Id):
+  
+    #get the board positions for the current lifeform
+    idX = posX
+    idY = posY
+  
+    #clear lists that will be used to temporarily store x, y info for other lifeforms on the board, for comparison
+    sItemX = ()
+    sItemY = ()
+
+    #for every item in the list of board positions perform a loop
+    for item in boardPositions:
+    
+        #split the items in the sub-list into seperate variables for comparison
+        sItemId = itemgetter(0)(item)
+        sItemX = itemgetter(1)(item)
+        sItemY = itemgetter(2)(item)
+
+        #if the id of the lifeform in the position list matches the id of the lifeform currently being checked, then do nothing - to prevent lifeforms from colliding with themselves
+        if sItemId == Id:
+            continue
+    
+        #if the x and y positions match that of a lifeform that is currently on the position list then return the id of the lifeform it collided with
+        elif idX == sItemX and idY == sItemY:
+            return sItemId
+
+#function for generating a new lifeform within the program      
+def generateLifeformAttribsSpark(Id, lifeSeed, lifeSeed2, lifeSeed3, posXGen, posYGen):
+    global holder
+    holder[Id].sparkLife(lifeSeed, lifeSeed2, lifeSeed3, posXGen, posYGen)  
+
+#function that assign lifeforms ids from the total number recieved by the function and puts them in a list
+def assignClasses(total):
+  
+    iList = []
+    for i in range(total):
+        i += 1
+        iList.append(i)
+  
+    return (iList)
+  
+def boardPositionGenerator():
+    posXGen = random.randint(0, 7)
+    posYGen = random.randint(0, 7)
+    return posXGen, posYGen
+
+def setup_game():
+    global iList
+    global holder
+    global settings
+
+    #obtain lifeform id list from the above function  
+    iList = assignClasses(settings.getValue(constants.NUM_LIFEFORMS_START))
+    #assign all the ids into class instances for each lifeform
+    holder = {Id: lifeForm(Id,settings.getValue(constants.MAX_AGRESSION),settings.getValue(constants.MAX_TTL)) for Id in iList}
+
+    #for each id in the list of all lifeform ids assign a random x and y number for the position on the board and create the new lifeform with random seeds for each lifeseed generation
+    for Id in iList:
+  
+        posXGen, posYGen = boardPositionGenerator()
+        #posXGen = random.randint(1, 8)
+        #posYGen = random.randint(1, 8)
+        generateLifeformAttribsSpark(Id, genRandom(), genRandom(), genRandom(), posXGen, posYGen)
 
 #### SETUP - called at webiopi startup
 def setup():
@@ -70,16 +162,64 @@ def setup():
     #load the settings file
     settings = Settings('/home/pi/Artificial_Life/')
     
+    #unicorn hat setup    
+    unicorn.set_layout(unicorn.AUTO)
+    unicorn.brightness(0.5)
+    unicorn.rotation(0)
+    unicorn.brightness(0.5)
+
     webiopi.info('setup complete')
 
 #### LOOP - main webiopi loop
 def loop():
     global is_running
-    
+    global settings
+    global iList
+    global holder
+
     if(is_running):
         webiopi.info("program is running")
+
+        #get the settings from the settings class
+        lifeFormTotal = settings.getValue(constants.NUM_LIFEFORMS_START)
+        popLimit = settings.getValue(constants.MAX_LIFEFORMS)
+        maxTTL = settings.getValue(constants.MAX_TTL)
+        maxAggro = settings.getValue(constants.MAX_AGRESSION)
+
+        clearLEDS()
+        posList = []
+
+        if iList:
+            for Id in iList:  
+                #clear colliderscope variable to make sure its fresh from data from the last iteration
+                colliderScope = ()
+                #call the movement function for the lifeform
+                holder[Id].movement()
+                #call expiry function for current lifeform and update the list of lifeforms
+                iList = holder[Id].expireEntity(iList)
+                #if the lifeform has expired then skip the loop as we dont want to continue processing an entity which is no longer on the board
+                if Id not in iList:
+                    continue
+                #print stats of current lifeform to console
+                holder[Id].getStats()
+                #assign variables from information about lifeform for use later
+                posX = holder[Id].matrixPositionX
+                posY = holder[Id].matrixPositionY
+                colR = holder[Id].redColor
+                colG = holder[Id].greenColor
+                colB = holder[Id].blueColor
+                #call function to draw leds with the current lifeforms x and y and r g b data, as well as the current layer
+                drawLEDS(posX, posY, colR, colG, colB)
+                #append the lifeforms id and x and y location to to the position list to be used by the collisiondetector
+                posList.append([Id, posX, posY])
+        elif not iList:
+            webiopi.info('Program done')
+            changeRunning('false')
+        #show LEDs
+        unicorn.show()
+        #sleep for the value set by the user
+        webiopi.sleep(settings.getValue(constants.SECONDS_BETWEEN_LOOPS))
     else:
         webiopi.info("program is not running")
+        webiopi.sleep(5)
 
-    #sleep for 5 seconds
-    webiopi.sleep(5)
